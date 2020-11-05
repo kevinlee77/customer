603438468b9c2f5488364f1e7bb2055bf65838cc
kevinlee77 <kevinlee77@naver.com>
2020-11-05 20:33:52 +0900

    1st commit

---
+++ src/main/java/bikeseller/Customer.java
@@ -0,0 +1,70 @@
+package bikeseller;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="Customer_table")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long orderId;
+    private String item;
+    private Integer qty;
+    private Double price;
+    private String status;
+    private Double point;
+
+
+    public Long getId() {
+            return id;
+        }
+
+    public void setId(Long id) {
+            this.id = id;
+        }
+    public Long getOrderId() {
+            return orderId;
+        }
+
+    public void setOrderId(Long orderId) {
+            this.orderId = orderId;
+        }
+    public String getItem() {
+            return item;
+        }
+
+    public void setItem(String item) {
+            this.item = item;
+        }
+    public Integer getQty() {
+            return qty;
+        }
+
+    public void setQty(Integer qty) {
+            this.qty = qty;
+        }
+    public Double getPrice() {
+            return price;
+        }
+
+    public void setPrice(Double price) {
+            this.price = price;
+        }
+    public String getStatus() {
+            return status;
+        }
+
+    public void setStatus(String status) {
+            this.status = status;
+        }
+    public Double getPoint() {
+        return point;
+    }
+
+    public void setPoint(Double point) {
+        this.point = point;
+    }
+}
---
+++ src/main/java/bikeseller/CustomerRepository.java
@@ -0,0 +1,12 @@
+package bikeseller;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByOrderId(Long orderId);
+
+}
